595b1d491a0922435f9d945616d4263960cc326b
Srinivas Renduchinthala <rcsrinivas@virtusa.com>
2022-08-29 12:59:23 +0530

    created project

---
+++ src/test/java/com/learning/springbootcurdoperations/SpringbootCurdOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springbootcurdoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCurdOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
